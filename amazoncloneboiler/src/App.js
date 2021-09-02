ed1e977fb144f37d4a752c3fc76587dfce972a10
Alex McDonald <acm4219@gmail.com>
2021-09-02 14:33:29 -0500

    made the header and styled it, gonna stop for today unless time later

--- amazoncloneboiler/src/App.js
+++ amazoncloneboiler/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Header from "./Header/Header"
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World</h1>
-
-      {/* Header */}
+      <Header />
       {/* Home */}
     </div>
   );
