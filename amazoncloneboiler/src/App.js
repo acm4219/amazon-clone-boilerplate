1de35e2ee67b44c269f1a624454e5084f9b7d209
Alex McDonald <acm4219@gmail.com>
2021-09-02 13:46:20 -0500

    added the header and home components and added css files to them

--- amazoncloneboiler/src/App.js
+++ amazoncloneboiler/src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Hello World</h1>
+
+      {/* Header */}
+      {/* Home */}
     </div>
   );
 }
