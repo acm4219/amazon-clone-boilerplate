d12cab4d54b60eacf2ce2716be28d8cc62ac3264
Alex McDonald <acm4219@gmail.com>
2021-09-02 13:32:47 -0500

    added connection to firebase

---
+++ amazoncloneboiler/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB_pzqoX1gDBmqJHbF3CpLSXHSP_gCMzvg",
+    authDomain: "clone-demo-98e49.firebaseapp.com",
+    projectId: "clone-demo-98e49",
+    storageBucket: "clone-demo-98e49.appspot.com",
+    messagingSenderId: "56887205977",
+    appId: "1:56887205977:web:060f14797cccce58801148",
+    measurementId: "G-NQN501N391"
+  };
